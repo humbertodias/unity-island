f5e98a47bdb2395eaf0556e45714ed6dce6c5fef
Humberto Dias <hldtux@gmail.com>
2016-12-01 14:21:23 -0200

    Zombie FBX.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+	public AudioSource fireSound;
+	public AudioClip reloadSound;
+	public AudioClip emptySound;
+	public GameObject weaponMesh;
+	public ParticleSystem bulletParticle;
+	private Vector3 initialPosition;
+
+	float cooldown = 0;
+	int magazine = 12;
+
+
+	Rigidbody rdbout;
+	RaycastHit hit;
+	Ray ray;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.localPosition;
+
+		// Esconde/Bloqueia cursor
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		weaponMesh.transform.localPosition = Vector3.Lerp (weaponMesh.transform.localPosition, initialPosition
+			, Time.deltaTime * 10);
+		cooldown += Time.deltaTime;
+
+		if (Input.GetButton ("Fire1") && !Input.GetKey (KeyCode.E) && cooldown > 0.1f) {
+
+			// Esconde/Bloqueia cursor
+			Cursor.lockState = CursorLockMode.Locked;
+
+			magazine--;
+			if (magazine > 0) {
+				shoot ();
+			} else {
+				fireSound.PlayOneShot (emptySound);
+			}
+				
+			cooldown = 0;
+		} 
+
+		if (Input.GetButtonDown ("Fire2") && magazine < 1){
+		
+			fireSound.PlayOneShot (reloadSound);
+			magazine = 12;
+			weaponMesh.transform.localPosition = new Vector3 (0, -10, -0.1f);
+		}
+
+
+}
+
+
+	void shoot(){
+
+		fireSound.PlayOneShot (fireSound.clip);
+
+		weaponMesh.transform.localPosition = new Vector3 (0, 0, 0.1f);
+		bulletParticle.Emit (100);
+
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//		ray = Camera.main.ScreenPointToRay (transform.position);
+//		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100)) {
+			// reposiciona emissor de particula
+			bulletParticle.transform.position = hit.point;
+
+			print ("Weapon.shot:" + hit.collider.gameObject.name);
+//			Rigidbody rdbout = hit.collider.gameObject.GetComponent<Rigidbody> ();
+			rdbout = hit.collider.gameObject.GetComponent<Rigidbody> ();
+			Invoke("Impact", hit.distance * 0.05f);
+
+		}
+	}
+
+	void Impact(){
+
+		print ("Weapon.Impact");
+		bulletParticle.transform.position = hit.point;
+		bulletParticle.Emit (30);
+		rdbout = hit.collider.gameObject.GetComponent<Rigidbody> ();
+		if (rdbout != null) {
+			rdbout.AddForceAtPosition (ray.direction * 200, hit.point);
+		}
+		print (hit.collider.name);
+//		hit.collider.gameObject.SendMessage ("kill", SendMessageOptions.DontRequireReceiver);
+		hit.collider.gameObject.SendMessageUpwards ("kill", SendMessageOptions.DontRequireReceiver);
+	}
+
+
+}
---
+++ Assets/Scripts/ZombieRag.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieRag : MonoBehaviour {
+
+	public Animator anim;
+	Rigidbody[] rdbs;
+	public NavMeshAgent agent;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		rdbs = GetComponentsInChildren<Rigidbody> ();
+		foreach (Rigidbody rdb in rdbs) {
+			rdb.isKinematic = true;
+		}
+	}
+
+	// evento
+	public void kill(){
+		print ("ZombieRag.kill");
+		anim.enabled = false;
+		agent.enabled = false;
+		foreach (Rigidbody rdb in rdbs) {
+			rdb.isKinematic = false;
+		}
+
+	}
+
+	// evento
+	void SetTarget(GameObject intarget){
+		target = intarget;
+	}
+
+
+	void OnTriggerEnter(Collider col){
+		print ("ZombieRag.OntriggerEnter:" + col.gameObject.name);
+		if (col.gameObject.name.Equals ("Player")) {
+			SetTarget (col.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (agent != null && target != null) {
+			print ("update follow");
+			agent.SetDestination (target.transform.position);
+		}
+	}
+}
